cd5f12d23dfd1cbd69dc862d85148a25311b7c29
Gary A. Stafford <garystafford@rochester.rr.com>
2019-01-23 00:05:10 -0500

    Final edits for post

--- part3_clean_up.sh
+++ part3_clean_up.sh
@@ -10,7 +10,6 @@ readonly PROJECT='ansible-demo-project'
 readonly ZONE='us-east1-b'
 readonly INSTANCE='compute-instance'
 
-# Delete instance
 time yes | gcloud compute instances delete $INSTANCE \
   --project $PROJECT --zone $ZONE
 
@@ -18,4 +17,4 @@ time yes | gcloud compute firewall-rules delete allow-http \
   --project $PROJECT
 
 # Clean up for next workflow
-rm ansible/inventories/hosts
+sh -c "echo '' > ansible/inventories/hosts"
